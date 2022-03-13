9b558bd7c0e063fd9e16c933ee800194d27929bd
hjc <hjc_0827@163.com>
2022-03-13 17:26:36 +0800

    refactor: 代码整理

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import Loader from './components/LazyPictures';
-import aPng from './assets/1.jpg';
+import dogImg from './assets/img.jpg';
 
 export default () => {
   return (
     <div style={{ display: 'flex', width: '100%', minWidth: "1200px" }}>
       <h1 style={{ width: '3000px', height: '600px' }}>1</h1>
-      <Loader style={{ width: '200px' }} src={aPng} />
+      <Loader style={{ width: '200px' }} src={dogImg} />
     </div>
   );
 };
